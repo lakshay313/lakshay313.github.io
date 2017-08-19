ebb4567b8364ae443b9b4a57734f2a9bd4a16336
lakshay313 <lakhshaybansal1998@gmail.com>
2017-08-19 08:18:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,16 @@ var element=document.getElementById('main-text');
 element.innerHTML="New value";
 //move image:
 var img=document.getElementById('madi');
-img.onclick=function(){
+/*img.onclick=function(){
     img.style.marginLeft='100px';
+};*/
+var marginLeft =0;
+
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
 };
