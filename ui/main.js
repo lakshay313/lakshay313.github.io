4e36a5d690446b936c6b7b4f02888f0f01a307a9
lakshay313 <lakhshaybansal1998@gmail.com>
2017-08-19 08:07:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change text using java script:
 var element=document.getElementById('main-text');
 element.innerHTML="New value";
+//move image:
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+}
