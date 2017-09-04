f784f947b4c13760ccca1003f2fc64f102d11d53
lakshay313 <lakhshaybansal1998@gmail.com>
2017-09-04 06:51:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,19 @@ button.onclick =function() {
     request.open('GET','http://lakhshaybansal199888.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+
+//SERVER NAME:
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function() {
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i< name.length; i++){
+        list += '<li>'+names[i]+'<li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHtml=list;
+}
