e0aaab354ab653c1c1b3b973635439b9852dfb6f
lakshay313 <lakhshaybansal1998@gmail.com>
2017-09-13 13:26:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+var config={
+    user: 'lakhshaybansal199888',
+    database: 'lakhshaybansal199888',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password : process.env.db-lakhshaybansal199888-33003
+};
 var app = express();
 app.use(morgan('combined'));
 var articles= { 
@@ -70,6 +78,22 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    // amke a select request 
+    // return a response with the results
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }
+      else{
+          res.send(JSON.stringify(result));
+      }
+       
+   }); 
+    
 });
 var counter=0;
 app.get('/counter', function (req,res) {
